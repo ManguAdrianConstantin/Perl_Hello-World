3fffbb3f41f0084544937e114d8145d993047087
ManguAdrianConstantin <44751747+ManguAdrianConstantin@users.noreply.github.com>
2019-03-09 17:05:09 +0200

    Add files via upload

---
+++ task_1/hello.pl
@@ -0,0 +1,14 @@
+#!/usr/local/bin/perl
+print "Perl first project\n";
+$i = 2019;
+@nume = "Adrian";
+print "@nume\n";
+if ($i > 2000){
+  print "It worked\n";
+}
+else{
+  print "Fail\n";
+}
+for (my $w = 0 ; $w <= $i; $w++){ #Sunt multe date de prelucrat cam 2019 :)
+  print "$w \n";
+}
